ec8da99756897b96c2a530e0f985e2f16d20ad1f
mmmjj <djkls7@naver.com>
2023-10-11 17:06:04 +0900

    query dsl 설정 추가,테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+//https://lemontia.tistory.com/1089
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QHello d = new QHello("h");
+
+        Hello s = queryFactory.selectFrom(d)
+                .fetchOne();
+
+        assertThat(s).isEqualTo(hello);
     }
 
 }
